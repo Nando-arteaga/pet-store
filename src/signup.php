2c8b2671136c88229f233c36291d784968bb488f
Nando <fernandoarteaga069@gmail.com>
2025-04-02 21:59:23 -0500

    Postma

---
+++ src/signup.php
@@ -0,0 +1,20 @@
+<?php
+
+include('config/database.php');
+
+$fname=$_POST['f_name'];
+$lname=$_POST['l_name'];
+$email=$_POST['e_mail'];
+$passw=$_POST['p_assw'];
+
+$sql= "INSERT INTO users(
+firstname,lastname,email,password)
+values ('$fname','$lname','$email','$passw')";
+$ans= pg_query ($conn,$sql);
+if ($ans){
+echo "user has been created succesfully";
+}else {
+echo "error";
+}
+
+?>
