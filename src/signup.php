7a57da3f61496d6bcbd4daab2af54a19747a0ffa
Nando <fernandoarteaga069@gmail.com>
2025-04-03 20:03:29 -0500

    Validate if email existent

--- src/signup.php
+++ src/signup.php
@@ -7,14 +7,24 @@ $lname=$_POST['l_name'];
 $email=$_POST['e_mail'];
 $passw=$_POST['p_assw'];
 
-$sql= "INSERT INTO users(
-firstname,lastname,email,password)
-values ('$fname','$lname','$email','$passw')";
-$ans= pg_query ($conn,$sql);
+$sql_validate_email= "
+select count (id) as total from users where email='$email'and status =true 
+";
+
+$ans= pg_query ($conn,$sql_validate_email);
 if ($ans){
-echo "user has been created succesfully";
-}else {
-echo "error";
-}
+    $row = pg_fetch_assoc($ans);
+if($row ['total'] > 0){
+    echo"user already exist";
+}else{
+    $sql= "INSERT INTO users(
+        firstname,lastname,email,password)
+        values ('$fname','$lname','$email','$passw')";
+        $ans= pg_query ($conn,$sql);
+        if ($ans){
+        echo "user has been created succesfully";
+        }else {
+        echo "error";
+        }}}
 
 ?>
