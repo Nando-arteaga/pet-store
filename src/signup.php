fa0ef8362244ee1d003780d5e219ce82b429112d
Nando <fernandoarteaga069@gmail.com>
2025-04-07 17:30:56 -0500

    validacion de usuario

--- src/signup.php
+++ src/signup.php
@@ -1,30 +1,33 @@
 <?php
 
-include('config/database.php');
+    include('config/database.php');
 
-$fname=$_POST['f_name'];
-$lname=$_POST['l_name'];
-$email=$_POST['e_mail'];
-$passw=$_POST['p_assw'];
+    $fname = $_POST['f_name'];
+    $lname = $_POST['l_name'];
+    $email = $_POST['e_mail'];
+    $passw = $_POST['p_assw'];
 
-$sql_validate_email= "
-select count (id) as total from users where email='$email'and status =true 
-";
+    $sql_validate_email= "
+        select count (id) as total from users where email = '$email' and status = true 
+    ";
 
-$ans= pg_query ($conn,$sql_validate_email);
-if ($ans){
-    $row = pg_fetch_assoc($ans);
-if($row ['total'] > 0){
-    echo"user already exist";
-}else{
-    $sql= "INSERT INTO users(
-        firstname,lastname,email,password)
-        values ('$fname','$lname','$email','$passw')";
-        $ans= pg_query ($conn,$sql);
-        if ($ans){
-        echo "user has been created succesfully";
-        }else {
-        echo "error";
-        }}}
-
+    $ans= pg_query ($conn,$sql_validate_email);
+    
+    if ($ans){
+        $row = pg_fetch_assoc($ans);
+        if($row ['total'] > 0){
+            echo "user already exist";
+        }else{
+            $sql= "INSERT INTO users(
+                    firstname,lastname,email,password)
+                        values ('$fname','$lname','$email','$passw')"
+            ;
+            $ans= pg_query ($conn,$sql);
+            if ($ans){
+                echo "user has been created succesfully";
+            }else {
+                echo "error";
+            }
+        }
+    }
 ?>
